a0dd370cdcefa531d099fffcca2fd86a45079254
André <tdc@gmx.ch>
2020-03-09 20:10:48 +0100

    change header useragent to chrome

---
+++ priceWatch.py
@@ -0,0 +1,19 @@
+import requests 
+import sys
+
+def getPrice(url):
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36"
+    }
+    response = requests.get(url, verify = False, headers = headers)
+    content = response.text
+    # print(content)
+    startIndex = content.find("<strong class=\"ZZdf\">")
+    print(f"start: {startIndex}")
+    price = content[startIndex:startIndex+35]
+
+    return price
+
+URL = sys.argv[1]
+price = getPrice(URL)
+print(price)
