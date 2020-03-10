669d085d2c62678429616cf8c9ecb7657733d1ec
André <tdc@gmx.ch>
2020-03-10 21:52:25 +0100

    HTMLParser

--- priceWatch.py
+++ priceWatch.py
@@ -1,5 +1,22 @@
 import requests 
 import sys
+from html.parser import HTMLParser
+
+class MyHTMLParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        if(tag == 'div'):
+            if "class=\"productDetail" in attrs:
+                print("attribute: ", attrs)
+
+    def handle_endtag(self, tag):
+        b = tag
+        # print("Encountered an end tag :", tag)
+
+    def handle_data(self, data):
+        b = data
+        # print("Encountered some data  :", data)
+
+parser = MyHTMLParser()
 
 def getPrice(url):
     headers = {
@@ -7,12 +24,15 @@ def getPrice(url):
     }
     response = requests.get(url, verify = False, headers = headers)
     content = response.text
+    parser.feed(content)
     # print(content)
-    startIndex = content.find("<strong class=\"ZZdf\">")
+    startIndex = content.find("<strong class=\"ZZcy\">")
     print(f"start: {startIndex}")
     price = content[startIndex:startIndex+35]
 
     return price
+
+
 
 URL = sys.argv[1]
 price = getPrice(URL)
