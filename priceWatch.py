6a04a4ae52c015b5b82142d04a390610e310f5aa
André <tdc@gmx.ch>
2020-03-13 17:42:03 +0100

    merged

--- priceWatch.py
+++ priceWatch.py
@@ -1,5 +1,6 @@
 import requests 
 import sys
+import re
 
 def getPrice(url):
     headers = {
@@ -7,13 +8,18 @@ def getPrice(url):
     }
     response = requests.get(url, verify = False, headers = headers)
     content = response.text
-    # print(content)
-    startIndex = content.find("<strong class=\"ZZdf\">")
-    print(f"start: {startIndex}")
-    price = content[startIndex:startIndex+35]
+    
+    # <div class="productDetail Z1a6"><header></header><div class="Z1ab"><strong class="ZZon"> 153.–</strong>
+    pattern = "<div class=\"productDetail [0-9a-zA-Z]{4}\"><header></header><div class=\"[0-9a-zA-Z]{4}\"><strong class=\"[0-9a-zA-Z]{4}\"> [0-9]{1,10}.–</strong>"
 
-    return price
+    result = re.findall(pattern, content)
+
+
+    if result:
+        print("Search successful.")
+        print(result)
+    else:
+        print("Search unsuccessful.")	
 
 URL = sys.argv[1]
-price = getPrice(URL)
-print(price)
+getPrice(URL)
