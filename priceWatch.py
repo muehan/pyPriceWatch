3009d84ed365aad3ab9c8d0b66561f659c1b41f9
André <tdc@gmx.ch>
2020-03-27 19:24:29 +0100

    working on regex

--- priceWatch.py
+++ priceWatch.py
@@ -8,11 +8,18 @@ def getPriceElement(url):
     }
     response = requests.get(url, verify = False, headers = headers)
     content = response.text
+
+    # print(content)
     
     # <div class="productDetail Z1a6"><header></header><div class="Z1ab"><strong class="ZZon"> 153.–</strong>
-    pattern = "<div class=\"productDetail [0-9a-zA-Z]{4}\"><header></header><div class=\"[0-9a-zA-Z]{4}\"><strong class=\"[0-9a-zA-Z]{4}\"> [0-9]{1,10}.–</strong>"
+    pattern = "<div class=\"productDetail [0-9a-zA-Z]{4}\"><header></header><div class=\"[0-9a-zA-Z]{4}\"><strong class=\"[0-9a-zA-Z]{4}\"> [0-9./–]{4,10}</strong>"
 
+    # current regex state
+    #<div class=\"productDetail [0-9a-zA-Z]{4}\"><header>\d*</header><div class=\"[0-9a-zA-Z]{4}\">.*<strong[0-9a-zA-Z=" ]{0,100}> [0-9./–]{4,10}</strong>
     result = re.findall(pattern, content)
+
+    print(result)
+
     match = result[0]
 
     if match:
@@ -33,3 +40,10 @@ URL = sys.argv[1]
 element = getPriceElement(URL)
 price = getPriceText(element)
 print(price)
+
+# demo data
+# <div class="productDetail Z18k"><header></header><div class="Z18p"><strong class="ZZe2"> 157.–</strong>
+
+# <div class="productDetail Z18h"><header></header><div class="Z18m"><strong class="ZZda"> 99.20</strong></div>
+
+# <div class="productDetail Z18h"><header><div class="ZZha"><div class="ZZpf ZZpg">–13%</div></div></header><div class="Z18m"><span class="ZZda ZZd8"><strong> 271.–</strong><span class="appendix ZZd9">statt <!-- --> 310.–</span></span></div>
